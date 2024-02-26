b22ac1b795dc51f91f1eac346bbeac2a78b4d1d8
Galih Prastya Juansaputra <galih.juansaputra@gmail.com>
2024-02-26 11:26:15 +0700

    belajar rest api

---
+++ src/main/java/com/enigma/enigma_shop/EnigmaShopApplication.java
@@ -0,0 +1,66 @@
+package com.enigma.enigma_shop;
+
+import com.enigma.enigma_shop.entity.Product;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+
+@SpringBootApplication
+@RestController
+public class EnigmaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnigmaShopApplication.class, args);
+	}
+
+	@GetMapping(path = "/hello-world", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String helloWorld(){
+		return "<h1>helloWorld</h1>";
+	}
+
+	@GetMapping(path = "/products", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String, Object> getProduct(){
+		return Map.of(
+				"id",1,
+				"name", "Apel",
+				"price", 3000
+		);
+	}
+
+	@GetMapping(path = "/menus")
+	public List<Map<String,Object>> getMenusFilter(
+			@RequestParam(name = "nama", required = false) String name,
+			@RequestParam(name = "maxPrice",required = false) Integer maxPrice
+			) {
+		Map<String, Object> menu = Map.of(
+				"id", 1,
+				"name", "Pentol",
+				"price", 3000
+		);
+		List<Map<String,Object>> menus = new ArrayList<>();
+		menus.add(menu);
+		return menus;
+	}
+
+	@GetMapping(path = "/menus/{id}")
+	public String getMenuById(@PathVariable(name = "id") String menuId) {
+		return "Product " + menuId;
+	}
+
+	@PostMapping(
+			path = "/products",
+			produces = MediaType.APPLICATION_JSON_VALUE,
+			consumes = MediaType.APPLICATION_JSON_VALUE
+	)
+	public Product createNewProduct(@RequestBody Product product) {
+		return product;
+	}
+
+
+}
---
+++ src/main/java/com/enigma/enigma_shop/entity/Product.java
@@ -0,0 +1,12 @@
+package com.enigma.enigma_shop.entity;
+
+import lombok.Data;
+
+@Data
+public class Product {
+
+    private String id;
+    private String name;
+    private Long Price;
+
+}
