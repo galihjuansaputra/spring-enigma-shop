172f8616b7c777b1d1c7ab6c46edd8fc60b0902f
Galih Prastya Juansaputra <galih.juansaputra@gmail.com>
2024-02-29 20:15:30 +0700

    Belajar Security Spring

---
+++ src/main/java/com/enigma/enigma_shop/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.enigma.enigma_shop.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity.
+                httpBasic(AbstractHttpConfigurer::disable)
+
+                /* Sisa config lainnya */
+
+                .build();
+    }
+}
